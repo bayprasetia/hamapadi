f8ee5d796cb7cbbddc08f122e8dcff3c65c275ff
Bayu Prasetia <142668959+bayprasetia@users.noreply.github.com>
2025-06-14 15:27:54 +0700

    index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ $action = isset($_GET['action']) ? $_GET['action'] : "";
 <html lang="id">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Sistem Pakar Hama dan Penyakit Padi</title>
   <link rel="stylesheet" href="Assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="Assets/css/datatables.min.css">
